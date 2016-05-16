031af2dac471bed4a6ddd65eb32a544470863f40
Dan Weiner <daweiner16@gmail.com>
2016-05-16 11:23:09 -0400

    add faster fiblist algorithm

--- fib_naive.py
+++ fib_naive.py
@@ -1,8 +1,24 @@
 # uses Python3
-def FibRecurs(n):
-    if n <= 1:
-        return n
-    else:
-        return FibRecurs(n - 1) + FibRecurs(n - 2)
 
-print(FibRecurs(20))
+# naive slow algorithm
+# def FibRecurs(n):
+#     if n <= 1:
+#         return n
+#     else:
+#         return FibRecurs(n - 1) + FibRecurs(n - 2)
+#
+# print(FibRecurs(20))
+
+
+# faster algorithm
+
+def FibList(n):
+    F = list(range(0,n+1))
+    if n <= 1:
+        F.append(n)
+    for i in range(2 , n + 1):
+        F[i] = F[i - 1] + F[i - 2]
+    return F[n]
+
+
+print(FibList(20))
